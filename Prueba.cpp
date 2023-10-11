21c08881678dd3ee7b2ebf49106c3ef0e85969d8
PDMontielC <147265912+PDMontielC@users.noreply.github.com>
2023-10-11 03:27:20 +0000

    Se agregaron menu's y opciones para el proyecto

--- Prueba.cpp
+++ Prueba.cpp
@@ -27,3 +27,46 @@ void Menu();
 void CargarDiccionario();
 string TraducirPalabra(const string &palabra);
 void TraducirCodigo();
+
+int main(){
+
+   Menu(); 
+
+}
+
+void Menu(){
+char opt;
+    cout<<"Bienvenido al proyecto de Algoritmos Grupo #3 2do Semestre 2023"<<endl;
+  do {
+    cout<<"Menu del programa:";
+      cout << "\nr. Registrar palabra\nv. Ver Diccionario\na. Actualizar palabra\nb. Borrar palabra\nt. Traducir\np. Participantes del grupo\ns. Salir\nSeleccione una opción: ";
+      cin >> opt;
+      switch (tolower(opt)) {
+          case 'r':
+              Crear();
+              break;
+          case 'v':
+              Leer();
+              break;
+          case 'a':
+              Actualizar();
+              break;
+          case 'b':
+              Borrar();
+              break;
+          case 't':
+              TraducirCodigo();
+              break;
+          case 'p':
+              Nosotros();
+              break;
+         default:
+            system("clear");
+            cout<<"Opcion ingresada no valida"<<endl;
+            break;
+      }
+  } while (opt != 's');
+    system("clear");
+    cout<<"Gracias por utilizar nuestro proyecto"<<endl;
+    system("pause") ;
+}
